118897f7e2ffc3e48d054cfee3c4b167df95c711
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-07-22 21:35:50 +0300

    start

---
+++ src/components/OfficeHoursList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const OfficeHoursList = () => {
+  return (
+    <Container className="p-2">
+      <h3 className="display-6 mb-2">OfficeHoursList </h3>
+    </Container>
+  );
+};
+
+export default OfficeHoursList;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import AppointmentList from "../components/AppointmentList";
+
+
+const Home = () => {
+  return (
+    <main className="text-center mt-2">
+      <h1 className="display-5 text-danger">BOZKAYA EDUCATION CENTER</h1>
+    
+      <AppointmentList />
+    </main>
+  );
+};
+
+export default Home;
