6152d0f4493eb33fc6973305186c1a6b754462e7
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-07-23 16:39:16 +0300

    data created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import AppointmentList from "../components/AppointmentList";
+
 
 
 const Home = () => {
@@ -6,7 +6,7 @@ const Home = () => {
     <main className="text-center mt-2">
       <h1 className="display-5 text-danger">BOZKAYA EDUCATION CENTER</h1>
     
-      <AppointmentList />
+ 
     </main>
   );
 };
