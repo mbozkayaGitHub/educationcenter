b0fae4a4e0507ccd755227fa210d5308b7fabff1
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-07-25 20:12:22 +0300

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+
+<h1 className="display-5 text-danger">BOZKAYA EDUCATION CENTER</h1>
+    </div>
+  )
+}
+
+export default Navbar
