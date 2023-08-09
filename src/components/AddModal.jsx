a22453bb246b698b24ed898db46f9712c99eca83
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-08-09 19:39:08 +0300

    Modal

---
+++ src/components/AddModal.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+
+const AddModal = () => {
+  return (
+    <>
+      <>
+        <Modal>
+          <Modal.Header closeButton>
+            <Modal.Title className="text-danger">
+              Reservation for ...
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="name">
+                <Form.Label>Student Name</Form.Label>
+                <Form.Control type="text" placeholder=" Enter your name" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="datetime">
+                <Form.Label>Day&Time</Form.Label>
+                <Form.Control type="datetime-local" />
+              </Form.Group>
+              <div className="text-center">
+                <Button variant="success" type="submit" className="me-2">
+                  Submit
+                </Button>
+                <Button variant="danger">Close</Button>
+              </div>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </>
+    </>
+  );
+};
+
+export default AddModal;
