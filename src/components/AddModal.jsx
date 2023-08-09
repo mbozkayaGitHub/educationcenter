f70d13485db0e3c23340a86f63d9d5949a6cda74
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-08-09 23:52:58 +0300

    Student&Date

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 
 const AddModal = ({show,handleClose,selectedTrName}) => {
+
+
+    const [studentName, setStudentName] = useState("")
+
+    const [date, setDate] = useState("")
 
 const handleSubmit = (e) => {
     e.preventDefault()
 
 }
 
-
+console.log(studentName,date);
   return (
     <>
       <>
@@ -22,18 +27,21 @@ const handleSubmit = (e) => {
             <Form onSubmit={handleSubmit}> 
               <Form.Group className="mb-3" controlId="name">
                 <Form.Label>Student Name</Form.Label>
-                <Form.Control type="text" placeholder=" Enter your name" />
+                <Form.Control type="text" placeholder=" Enter your name"
+                onChange={(e)=>setStudentName(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="datetime">
                 <Form.Label>Day&Time</Form.Label>
-                <Form.Control type="datetime-local" />
+                <Form.Control type="datetime-local" 
+                  onChange={(e) =>setDate(e.target.value)}/>
               </Form.Group>
               <div className="text-center">
                 <Button variant="success" type="submit" className="me-2">
                   Submit
                 </Button>
-                <Button variant="danger">Close</Button>
+                <Button variant="danger" onClick={handleClose}>Close</Button>
               </div>
             </Form>
           </Modal.Body>
