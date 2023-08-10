cf6d020d73285455b5ccc06181fb1c65aab180bc
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-08-10 22:02:31 +0300

    OfficeHours

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,17 @@ import { useState } from "react";
 import Lecturers from "../components/Lecturers";
 import { lecturerData } from "../helper/data";
 import Navbar from "../components/Navbar";
-
-
-
-
+import OfficeHoursList from "../components/OfficeHoursList";
 
 const Home = () => {
-const [lecturers, setLecturers] = useState(lecturerData)
+  const [lecturers, setLecturers] = useState(lecturerData);
 
   return (
     <main className="text-center mt-2">
-      <Navbar/>
-    
-    
-   <Lecturers  lecturers={lecturers}/>
+      <Navbar />
+
+      <Lecturers lecturers={lecturers} />
+      <OfficeHoursList/>
     </main>
   );
 };
