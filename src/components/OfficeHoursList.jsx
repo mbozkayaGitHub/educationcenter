77bb87c7020b58b6c93f2dbb2c0170efbc1d2b79
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-08-12 21:36:24 +0300

    onScreen

--- src/components/OfficeHoursList.jsx
+++ src/components/OfficeHoursList.jsx
@@ -7,6 +7,15 @@ const OfficeHoursList = ({ officeHoursList, setOfficeHoursList }) => {
 const handleDelete = (id) => {
   setOfficeHoursList(officeHoursList.filter((item)=> item.id !== id));
 }
+
+const handleDoubleClick = (id) => {
+  setOfficeHoursList(
+    officeHoursList.map((item)=> 
+      item.id === id ? {...item,consulted:
+        !item.consulted} : item
+    )
+  )
+}
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2">OfficeHoursList </h3>
@@ -21,21 +30,26 @@ const handleDelete = (id) => {
           <div
             key={id}
             className={
-              consulted ? "officeHoursList consulted" : "officeHoursList "
+              consulted ? "officeHoursList consulted" : "officeHoursList"
             }
+            onDoubleClick={() => handleDoubleClick(id)}
           >
             <Row>
               <Col>
                 <h4 className="text-danger">{student}</h4>
                 <h5>{lecturer}</h5>
               </Col>
-              <Col>
+              <Col >
                 <h6>{new Date(day).toLocaleDateString()}</h6>
                 <h6>{new Date(day).toLocaleTimeString()}</h6>
               </Col>
+              <Col
+              className="text-end"
+              > 
               <FaTimesCircle className="text-danger fs-3" type="button" 
                onClick={() => handleDelete(id)}
               />
+              </Col>
             </Row>
           </div>
         );
