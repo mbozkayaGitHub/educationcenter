eb6cfd6a3d320b39b3f13b695a6115f9000a9f35
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-08-12 20:46:48 +0300

    student&lecturer

--- src/components/OfficeHoursList.jsx
+++ src/components/OfficeHoursList.jsx
@@ -6,17 +6,17 @@ const OfficeHoursList = ({officeHoursList, setOfficeHoursList}) => {
     <Container className="p-2">
       
       <h3 className="display-6 mb-2">OfficeHoursList </h3>
-    {officeHoursList.map(()=>{
-
+    {officeHoursList.map((item)=>{
+const { id, student, consulted, lecturer, day } = item
       return (
         <div>
           <Row>
             <Col>
-            <h4>student</h4>
-            <h5>lecturer</h5>
+            <h4>{student}</h4>
+            <h5>{lecturer}</h5>
             </Col>
-            <h6>{new Date().toLocaleDateString()}</h6>
-                <h6>{new Date().toLocaleTimeString()}</h6>
+            <h6>{new Date(day).toLocaleDateString()}</h6>
+                <h6>{new Date(day).toLocaleTimeString()}</h6>
           </Row>
         </div>
       )
