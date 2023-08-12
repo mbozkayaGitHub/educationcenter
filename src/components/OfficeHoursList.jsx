a50e80f6257d90dafe49346f63fe19d4a5bcd3a1
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-08-12 21:08:05 +0300

    img

--- src/components/OfficeHoursList.jsx
+++ src/components/OfficeHoursList.jsx
@@ -1,26 +1,45 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { FaTimesCircle } from "react-icons/fa";
 
-const OfficeHoursList = ({officeHoursList, setOfficeHoursList}) => {
+const OfficeHoursList = ({ officeHoursList, setOfficeHoursList }) => {
+
+const handleDelete = (id) => {
+  setOfficeHoursList(officeHoursList.filter((item)=> item.id !== id));
+}
   return (
     <Container className="p-2">
-      
       <h3 className="display-6 mb-2">OfficeHoursList </h3>
-    {officeHoursList.map((item)=>{
-const { id, student, consulted, lecturer, day } = item
-      return (
-        <div>
-          <Row>
-            <Col>
-            <h4>{student}</h4>
-            <h5>{lecturer}</h5>
-            </Col>
-            <h6>{new Date(day).toLocaleDateString()}</h6>
+{officeHoursList.length < 1 && (
+  <img src="./img/officeHours.jpg" alt="" width="50%"/>
+)}
+
+
+      {officeHoursList.map((item) => {
+        const { id, student, consulted, lecturer, day } = item;
+        return (
+          <div
+            key={id}
+            className={
+              consulted ? "officeHoursList consulted" : "officeHoursList "
+            }
+          >
+            <Row>
+              <Col>
+                <h4 className="text-danger">{student}</h4>
+                <h5>{lecturer}</h5>
+              </Col>
+              <Col>
+                <h6>{new Date(day).toLocaleDateString()}</h6>
                 <h6>{new Date(day).toLocaleTimeString()}</h6>
-          </Row>
-        </div>
-      )
-    })}
+              </Col>
+              <FaTimesCircle className="text-danger fs-3" type="button" 
+               onClick={() => handleDelete(id)}
+              />
+            </Row>
+          </div>
+        );
+      })}
     </Container>
   );
 };
