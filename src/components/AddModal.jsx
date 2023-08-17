13c63e45262fececd05e1551b9fe94ec639900d4
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-08-17 22:41:00 +0300

    addModal

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,64 +1,78 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
-import OfficeHoursList from "./OfficeHoursList";
 
-const AddModal = ({show,handleClose,selectedTrName,setOfficeHoursList,officeHoursList}) => {
+function AddModal({
+  show,
+  handleClose,
+  selectedTrName,
+  setOfficeHoursList,
+  officeHoursList,
+}) {
+  const [studentName, setStudentName] = useState("");
+
+  const [date, setDate] = useState("");
 
 
-    const [studentName, setStudentName] = useState("")
+  console.log(setOfficeHoursList);
 
-    const [date, setDate] = useState("")
+  console.log(selectedTrName);
+ console.log(studentName, date);
 
-const handleSubmit = (e) => {
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
     setOfficeHoursList([
-      ...officeHoursList, {
-        id:officeHoursList.length + 1,
-        student:studentName,
-        day:date,
-        cosulted:false,
-        lecturer:selectedTrName,
-      }
-    ])
-handleClose()
-}
-console.log(selectedTrName);
-console.log(studentName,date);
+      ...officeHoursList,
+      {
+        id: officeHoursList.length + 1,
+        student: studentName,
+        day: date,
+        consulted: false,
+        lecturer: selectedTrName,
+      },
+    ]);
+
+    handleClose();
+  };
+
   return (
     <>
-      <>
-        <Modal  show={show} onHide={handleClose} >
-          <Modal.Header closeButton>
-            <Modal.Title className="text-danger">
-              Reservation for {selectedTrName}
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form onSubmit={handleSubmit}> 
-              <Form.Group className="mb-3" controlId="name">
-                <Form.Label>Student Name</Form.Label>
-                <Form.Control type="text" placeholder=" Enter your name"
-                onChange={(e)=>setStudentName(e.target.value)}
-                />
-              </Form.Group>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className="text-danger">
+            Reservation for {selectedTrName}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Student Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder=" Enter your name"
+                onChange={(e) => setStudentName(e.target.value)}
+              />
+            </Form.Group>
 
-              <Form.Group className="mb-3" controlId="datetime">
-                <Form.Label>Day&Time</Form.Label>
-                <Form.Control type="datetime-local" 
-                  onChange={(e) =>setDate(e.target.value)}/>
-              </Form.Group>
-              <div className="text-center">
-                <Button variant="success" type="submit" className="me-2">
-                  Submit
-                </Button>
-                <Button variant="danger" onClick={handleClose}>Close</Button>
-              </div>
-            </Form>
-          </Modal.Body>
-        </Modal>
-      </>
+            <Form.Group className="mb-3" controlId="datetime">
+              <Form.Label>Day&Time</Form.Label>
+              <Form.Control
+                type="datetime-local"
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </Form.Group>
+            <div className="text-center">
+              <Button variant="success" type="submit" className="me-2">
+                Submit
+              </Button>
+              <Button variant="danger" onClick={handleClose}>
+                Close
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
   );
-};
+}
 
 export default AddModal;
