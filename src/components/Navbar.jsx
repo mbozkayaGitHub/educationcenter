4505558d8e385b3e2d917ea8adea43f7754dcb6a
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-08-29 22:04:33 +0300

    fontweight

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,13 @@ import React from "react";
 
 const Navbar = () => {
   return (
-
     <div>
-      <div className="bg-primary text-white opacity-75">
-    <h1 className="display-5 ">
-      BOZKAYA EDUCATION CENTER
-    </h1>
+      <div className="shadow p-3 bg-primary text-white opacity-75 ">
+        <h1 className="display-5 fw-normal">BOZKAYA EDUCATION CENTER</h1>
 
-    <h3 className="display-6 mb-3">Lecturers</h3>
-  </div> </div>
-    
+        <h3 className="display-6 mb-3 fw-bolder text-warning">Lecturers</h3>
+      </div>{" "}
+    </div>
   );
 };
 
