bec60c99b97421b8850b0092436423382329d958
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-08-29 21:31:38 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,15 @@ import React from "react";
 const Navbar = () => {
   return (
 
-    
-    <div className="bg-primary text-white ">
-      <h1 className="display-5 ">
-        BOZKAYA EDUCATION CENTER
-      </h1>
+    <div>
+      <div className="bg-primary text-white ">
+    <h1 className="display-5 ">
+      BOZKAYA EDUCATION CENTER
+    </h1>
 
-      <h3 className="display-6 mb-3">Lecturers</h3>
-    </div>
+    <h3 className="display-6 mb-3">Lecturers</h3>
+  </div> </div>
+    
   );
 };
 
