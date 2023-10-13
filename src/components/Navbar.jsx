606e5229056744cfc18155b6fc9ff6f902d65e14
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-10-13 21:04:37 +0300

    branch completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="shadow p-4 bg-info text-white opacity-75" height="50%">
         <div className="text-start"> <img src="../img/education.jpg" class="rounded-circle" width="10%" alt="" />
         </div>
-        <h1 className="display-5 fw-normal">BOZKAYA EDUCATION CENTER</h1>
+        <h1 className="display-5 fw-normal">BOZKAYA EDUCATION CENTER!</h1>
 
         <h3 className="display-6 mb-0 fw-bolder text-warning ">Lecturers</h3>
       </div>
